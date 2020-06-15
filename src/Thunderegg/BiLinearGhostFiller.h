46b1c014d9062503e25d9673e5f96ebbafadc956
Scott Aiton <scott@aiton.net>
2020-06-15 12:11:56 -0600

    Fixed BiLinearGhostFiller

--- src/Thunderegg/BiLinearGhostFiller.h
+++ src/Thunderegg/BiLinearGhostFiller.h
@@ -60,18 +60,15 @@ class BiLinearGhostFiller : public MPIGhostFiller<2>
 			case NbrType::Fine: {
 				auto nbr_info    = pinfo->getFineNbrInfo(side);
 				auto local_slice = local_data.getSliceOnSide(side);
-				for (int nbr_patch = 0; nbr_patch < 2; nbr_patch++) {
-					auto nbr_ghosts = nbr_data.getGhostSliceOnSide(side.opposite(), 1);
-					int  offset     = 0;
-					if (nbr_patch == 1) {
-						offset = pinfo->ns[!side.axis()];
-					}
-					nested_loop<1>(nbr_ghosts.getStart(), nbr_ghosts.getEnd(),
-					               [&](const std::array<int, 1> &coord) {
-						               nbr_ghosts[coord]
-						               += 2.0 / 3.0 * local_slice[{(coord[0] + offset) / 2}];
-					               });
+				auto nbr_ghosts  = nbr_data.getGhostSliceOnSide(side.opposite(), 1);
+				int  offset      = 0;
+				if (orthant.collapseOnAxis(side.axis()) == Orthant<1>::upper) {
+					offset = pinfo->ns[!side.axis()];
 				}
+				nested_loop<1>(
+				nbr_ghosts.getStart(), nbr_ghosts.getEnd(), [&](const std::array<int, 1> &coord) {
+					nbr_ghosts[coord] += 2.0 / 3.0 * local_slice[{(coord[0] + offset) / 2}];
+				});
 			} break;
 		}
 	}
@@ -120,93 +117,6 @@ class BiLinearGhostFiller : public MPIGhostFiller<2>
 	{
 	}
 
-	/**
-	 * @brief Fill the ghost cells on a vector
-	 *
-	 * @param u  the vector
-	 */
-	void fillGhost(std::shared_ptr<const Vector<2>> u) const
-	{
-		for (auto pinfo : domain->getPatchInfoVector()) {
-			const LocalData<2> this_patch = u->getLocalData(pinfo->local_index);
-			for (Side<2> s : Side<2>::getValues()) {
-				if (pinfo->hasNbr(s)) {
-					auto this_ghost = this_patch.getGhostSliceOnSide(s, 1);
-					nested_loop<1>(this_ghost.getStart(), this_ghost.getEnd(),
-					               [&](const std::array<int, 1> &coord) { this_ghost[coord] = 0; });
-				}
-			}
-		}
-		for (auto pinfo : domain->getPatchInfoVector()) {
-			for (Side<2> s : Side<2>::getValues()) {
-				if (pinfo->hasNbr(s)) {
-					switch (pinfo->getNbrType(s)) {
-						case NbrType::Normal: {
-							auto               nbr_info    = pinfo->getNormalNbrInfo(s);
-							const LocalData<2> this_patch  = u->getLocalData(pinfo->local_index);
-							const LocalData<2> other_patch = u->getLocalData(nbr_info.local_index);
-							auto               this_side   = this_patch.getSliceOnSide(s);
-							auto other_side = other_patch.getGhostSliceOnSide(s.opposite(), 1);
-							nested_loop<1>(this_side.getStart(), this_side.getEnd(),
-							               [&](const std::array<int, 1> &coord) {
-								               other_side[coord] = this_side[coord];
-							               });
-						} break;
-						case NbrType::Coarse: {
-							auto               nbr_info    = pinfo->getCoarseNbrInfo(s);
-							const LocalData<2> this_patch  = u->getLocalData(pinfo->local_index);
-							const LocalData<2> other_patch = u->getLocalData(nbr_info.local_index);
-							auto               this_side   = this_patch.getSliceOnSide(s);
-							auto               this_ghost  = this_patch.getGhostSliceOnSide(s, 1);
-							auto other_side = other_patch.getGhostSliceOnSide(s.opposite(), 1);
-							int  offset     = 0;
-							if (nbr_info.orth_on_coarse.toInt() == 1) {
-								offset = pinfo->ns[!s.axis()];
-							}
-							nested_loop<1>(this_side.getStart(), this_side.getEnd(),
-							               [&](const std::array<int, 1> &coord) {
-								               this_ghost[coord] += 2.0 / 3.0 * this_side[coord];
-								               if ((coord[0] + offset) % 2 == 0) {
-									               this_ghost[{coord[0] + 1}]
-									               += -1.0 / 3.0 * this_side[coord];
-								               } else {
-									               this_ghost[{coord[0] - 1}]
-									               += -1.0 / 3.0 * this_side[coord];
-								               }
-								               other_side[{(coord[0] + offset) / 2}]
-								               += 2.0 / 3.0 * this_side[coord];
-							               });
-						} break;
-						case NbrType::Fine: {
-							auto               nbr_info   = pinfo->getFineNbrInfo(s);
-							const LocalData<2> this_patch = u->getLocalData(pinfo->local_index);
-							auto               this_side  = this_patch.getSliceOnSide(s);
-							for (int nbr_patch = 0; nbr_patch < 2; nbr_patch++) {
-								const LocalData<2> other_patch
-								= u->getLocalData(nbr_info.local_indexes[nbr_patch]);
-								auto other_side = other_patch.getGhostSliceOnSide(s.opposite(), 1);
-								int  offset     = 0;
-								if (nbr_patch == 1) {
-									offset = pinfo->ns[!s.axis()];
-								}
-								nested_loop<1>(other_side.getStart(), other_side.getEnd(),
-								               [&](const std::array<int, 1> &coord) {
-									               other_side[coord]
-									               += 2.0 / 3.0
-									                  * this_side[{(coord[0] + offset) / 2}];
-								               });
-							}
-							auto this_ghost = this_patch.getGhostSliceOnSide(s, 1);
-							nested_loop<1>(this_ghost.getStart(), this_ghost.getEnd(),
-							               [&](const std::array<int, 1> &coord) {
-								               this_ghost[coord] += -1.0 / 3.0 * this_side[coord];
-							               });
-						} break;
-					}
-				}
-			}
-		}
-	}
 	/**
 	 * @brief Generates ghost fillers for a given GMG level
 	 */
