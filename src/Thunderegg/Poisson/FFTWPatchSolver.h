507f92bcf4a44e23fc9c7f47b40a418658d454ee
Scott Aiton <scott@aiton.net>
2020-07-30 14:34:16 -0600

    (#21) FFTWPatchSolver works

--- src/Thunderegg/Poisson/FFTWPatchSolver.h
+++ src/Thunderegg/Poisson/FFTWPatchSolver.h
@@ -40,10 +40,6 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 {
 	private:
 	/**
-	 * @brief Enum of DFT types
-	 */
-	enum DftType { DCT_II, DCT_III, DCT_IV, DST_II, DST_III, DST_IV };
-	/**
 	 * @brief Comparator used in the maps, patches with the same spacings and boundary conditions
 	 * will be equal
 	 */
@@ -62,15 +58,11 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	/**
 	 * @brief Map of patchinfo to DFT plan
 	 */
-	std::map<std::shared_ptr<const PatchInfo<D>>,
-	         std::array<std::shared_ptr<std::valarray<double>>, D>, CompareByBoundaryAndSpacings>
-	plan1;
+	std::map<std::shared_ptr<const PatchInfo<D>>, fftw_plan, CompareByBoundaryAndSpacings> plan1;
 	/**
 	 * @brief Map of patchinfo to inverse DFT plan
 	 */
-	std::map<std::shared_ptr<const PatchInfo<D>>,
-	         std::array<std::shared_ptr<std::valarray<double>>, D>, CompareByBoundaryAndSpacings>
-	plan2;
+	std::map<std::shared_ptr<const PatchInfo<D>>, fftw_plan, CompareByBoundaryAndSpacings> plan2;
 	/**
 	 * @brief Temporary copy for the modified right hand side
 	 */
@@ -80,9 +72,9 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	 */
 	std::shared_ptr<ValVector<D>> tmp;
 	/*
-	 * @brief Temporary work vector
+	 * @brief Temporary work vector for solution
 	 */
-	std::shared_ptr<ValVector<D>> local_tmp;
+	std::shared_ptr<ValVector<D>> sol;
 	/**
 	 * @brief Map of PatchInfo object to it's respective eigenvalue array.
 	 */
@@ -90,157 +82,53 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	         CompareByBoundaryAndSpacings>
 	eigen_vals;
 	/**
-	 * @brief map of DFT transforms for each type and size
-	 */
-	std::map<std::tuple<DftType, int>, std::shared_ptr<std::valarray<double>>> transform_matrixes;
-
-	/**
-	 * @brief get arrays of coefficients necessary for each transform.
-	 *
-	 * @param types the tranforms for each axis
-	 * @return std::array<std::shared_ptr<std::valarray<double>>, D> the coefficients for each
-	 * transform
-	 */
-	std::array<std::shared_ptr<std::valarray<double>>, D> plan(std::array<DftType, D> types)
-	{
-		std::array<std::shared_ptr<std::valarray<double>>, D> retval;
-		for (size_t axis = 0; axis < D; axis++) {
-			retval[axis] = getTransformArray(types[axis], this->domain->getNs()[axis]);
-		}
-		return retval;
-	}
-
-	/**
-	 * @brief Get a dft Transform matrix for a certain type and size
-	 *
-	 * @param type the DFT type
-	 * @param n the size of the matrix
-	 * @return std::shared_ptr<std::valarray<double>> an nxn DFT matrix
-	 */
-	std::shared_ptr<std::valarray<double>> getTransformArray(DftType type, int n)
-	{
-		std::shared_ptr<std::valarray<double>> matrix_ptr;
-
-		if (transform_matrixes.count(std::make_tuple(type, n)) == 0) {
-			matrix_ptr.reset(new std::valarray<double>(n * n));
-			std::valarray<double> &matrix = *matrix_ptr;
-			switch (type) {
-				case DftType::DCT_II:
-					for (int j = 0; j < n; j++) {
-						for (int i = 0; i < n; i++) {
-							matrix[i * n + j] = cos(M_PI / n * (i * (j + 0.5)));
-						}
-					}
-					break;
-				case DftType::DCT_III:
-					for (int i = 0; i < n; i++) {
-						matrix[i * n] = 0.5;
-					}
-					for (int j = 1; j < n; j++) {
-						for (int i = 0; i < n; i++) {
-							matrix[i * n + j] = cos(M_PI / n * ((i + 0.5) * j));
-						}
-					}
-					break;
-				case DftType::DCT_IV:
-					for (int j = 0; j < n; j++) {
-						for (int i = 0; i < n; i++) {
-							matrix[i * n + j] = cos(M_PI / n * ((i + 0.5) * (j + 0.5)));
-						}
-					}
-					break;
-				case DftType::DST_II:
-					for (int i = 0; i < n; i++) {
-						for (int j = 0; j < n; j++) {
-							matrix[i * n + j] = sin(M_PI / n * ((i + 1) * (j + 0.5)));
-						}
-					}
-					break;
-				case DftType::DST_III:
-					for (int i = 0; i < n; i += 2) {
-						matrix[i * n + n - 1] = 0.5;
-					}
-					for (int i = 1; i < n; i += 2) {
-						matrix[i * n + n - 1] = -0.5;
-					}
-					for (int i = 0; i < n; i++) {
-						for (int j = 0; j < n - 1; j++) {
-							matrix[i * n + j] = sin(M_PI / n * ((i + 0.5) * (j + 1)));
-						}
-					}
-					break;
-				case DftType::DST_IV:
-					for (int j = 0; j < n; j++) {
-						for (int i = 0; i < n; i++) {
-							matrix[i * n + j] = sin(M_PI / n * ((i + 0.5) * (j + 0.5)));
-						}
-					}
-				default:
-					break;
-			}
-		} else {
-			matrix_ptr = transform_matrixes.at(std::make_tuple(type, n));
-		}
-		return matrix_ptr;
-	}
-	/**
-	 * @brief Execute a given DFT plan
-	 *
-	 * @param plan the plan (the matrixes for each axis)
-	 * @param in the input values
-	 * @param out the resulting values after the transform
-	 * @param inverse weather we a re calculate
-	 */
-	void executePlan(const std::array<std::shared_ptr<std::valarray<double>>, D> &plan,
-	                 LocalData<D> in, LocalData<D> out) const
-	{
-	}
-	/**
-	 * @brief Get the dft transform types for a patch
+	 * @brief Get the fft transform types for a patch
 	 *
 	 * @param pinfo the patch
-	 * @return std::array<DftType, D> an array of tranforms for each axis
+	 * @return std::array<fftw_r2r_kind, D> an array of tranforms for each axis, the order of
+	 * dimensions is reversed because FFTW uses row-major format
 	 */
-	std::array<DftType, D>
+	std::array<fftw_r2r_kind, D>
 	getTransformsForPatch(std::shared_ptr<const Thunderegg::PatchInfo<D>> pinfo)
 	{
 		// get transform types for each axis
-		std::array<DftType, D> transforms;
+		std::array<fftw_r2r_kind, D> transforms;
 		for (size_t axis = 0; axis < D; axis++) {
 			if (pinfo->isNeumann(Side<D>::LowerSideOnAxis(axis))
 			    && pinfo->isNeumann(Side<D>::HigherSideOnAxis(axis))) {
-				transforms[axis] = DftType::DCT_II;
+				transforms[D - 1 - axis] = FFTW_REDFT10;
 			} else if (pinfo->isNeumann(Side<D>::LowerSideOnAxis(axis))) {
-				transforms[axis] = DftType::DCT_IV;
+				transforms[D - 1 - axis] = FFTW_REDFT11;
 			} else if (pinfo->isNeumann(Side<D>::HigherSideOnAxis(axis))) {
-				transforms[axis] = DftType::DST_IV;
+				transforms[D - 1 - axis] = FFTW_RODFT11;
 			} else {
-				transforms[axis] = DftType::DST_II;
+				transforms[D - 1 - axis] = FFTW_RODFT10;
 			}
 		}
 		return transforms;
 	}
 	/**
-	 * @brief Get the inverse dft transform types for a patch
+	 * @brief Get the inverse fft transform types for a patch
 	 *
 	 * @param pinfo the patch
-	 * @return std::array<DftType, D> an array of tranforms for each axis
+	 * @return std::array<fftw_r2r_kind, D> an array of tranforms for each axis, the order of
+	 * dimensions is reversed because FFTW uses row-major format
 	 */
-	std::array<DftType, D>
+	std::array<fftw_r2r_kind, D>
 	getInverseTransformsForPatch(std::shared_ptr<const Thunderegg::PatchInfo<D>> pinfo)
 	{
 		// get transform types for each axis
-		std::array<DftType, D> transforms_inv;
+		std::array<fftw_r2r_kind, D> transforms_inv;
 		for (size_t axis = 0; axis < D; axis++) {
 			if (pinfo->isNeumann(Side<D>::LowerSideOnAxis(axis))
 			    && pinfo->isNeumann(Side<D>::HigherSideOnAxis(axis))) {
-				transforms_inv[axis] = DftType::DCT_III;
+				transforms_inv[D - 1 - axis] = FFTW_REDFT01;
 			} else if (pinfo->isNeumann(Side<D>::LowerSideOnAxis(axis))) {
-				transforms_inv[axis] = DftType::DCT_IV;
+				transforms_inv[D - 1 - axis] = FFTW_REDFT11;
 			} else if (pinfo->isNeumann(Side<D>::HigherSideOnAxis(axis))) {
-				transforms_inv[axis] = DftType::DST_IV;
+				transforms_inv[D - 1 - axis] = FFTW_RODFT11;
 			} else {
-				transforms_inv[axis] = DftType::DST_III;
+				transforms_inv[D - 1 - axis] = FFTW_RODFT01;
 			}
 		}
 		return transforms_inv;
@@ -315,8 +203,19 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	void addPatch(std::shared_ptr<const PatchInfo<D>> pinfo)
 	{
 		if (plan1.count(pinfo) == 0) {
-			plan1[pinfo]      = plan(getTransformsForPatch(pinfo));
-			plan2[pinfo]      = plan(getInverseTransformsForPatch(pinfo));
+			// revers ns because FFTW is row major
+			std::array<int, D> ns_reversed;
+			for (size_t i = 0; i < D; i++) {
+				ns_reversed[D - 1 - i] = pinfo->ns[i];
+			}
+			std::array<fftw_r2r_kind, D> transforms     = getTransformsForPatch(pinfo);
+			std::array<fftw_r2r_kind, D> transforms_inv = getInverseTransformsForPatch(pinfo);
+
+			plan1[pinfo] = fftw_plan_r2r(D, ns_reversed.data(), &f_copy->vec[0], &tmp->vec[0],
+			                             transforms.data(), FFTW_MEASURE | FFTW_DESTROY_INPUT);
+			plan2[pinfo] = fftw_plan_r2r(D, ns_reversed.data(), &tmp->vec[0], &sol->vec[0],
+			                             transforms_inv.data(), FFTW_MEASURE | FFTW_DESTROY_INPUT);
+
 			eigen_vals[pinfo] = getEigenValues(pinfo);
 		}
 	}
@@ -332,9 +231,7 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	{
 		f_copy = std::make_shared<ValVector<D>>(MPI_COMM_SELF, this->domain->getNs(), 0, 1);
 		tmp    = std::make_shared<ValVector<D>>(MPI_COMM_SELF, this->domain->getNs(), 0, 1);
-		if (!(D % 2)) {
-			local_tmp = std::make_shared<ValVector<D>>(MPI_COMM_SELF, this->domain->getNs(), 0, 1);
-		}
+		sol    = std::make_shared<ValVector<D>>(MPI_COMM_SELF, this->domain->getNs(), 0, 1);
 		// process patches
 		for (auto pinfo : this->domain->getPatchInfoVector()) {
 			addPatch(pinfo);
@@ -344,14 +241,13 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 	                      const LocalData<D> f) const override
 	{
 		LocalData<D> f_copy_ld = f_copy->getLocalData(0);
-		LocalData<D> tmp_ld    = tmp->getLocalData(0);
 
 		nested_loop<D>(f_copy_ld.getStart(), f_copy_ld.getEnd(),
 		               [&](std::array<int, D> coord) { f_copy_ld[coord] = f[coord]; });
 
 		op->addGhostToRHS(pinfo, u, f_copy_ld);
 
-		executePlan(plan1.at(pinfo), f_copy_ld, tmp_ld);
+		fftw_execute(plan1.at(pinfo));
 
 		tmp->vec /= eigen_vals.at(pinfo);
 
@@ -359,190 +255,18 @@ template <size_t D> class FFTWPatchSolver : public PatchSolver<D>
 			tmp->vec[0] = 0;
 		}
 
-		executePlan(plan2.at(pinfo), tmp_ld, u);
+		fftw_execute(plan2.at(pinfo));
+
+		LocalData<D> sol_ld = sol->getLocalData(0);
 
 		double scale = 1;
 		for (size_t axis = 0; axis < D; axis++) {
-			scale *= 2.0 / this->domain->getNs()[axis];
+			scale *= 2.0 * this->domain->getNs()[axis];
 		}
 		nested_loop<D>(u.getStart(), u.getEnd(),
-		               [&](std::array<int, D> coord) { u[coord] *= scale; });
+		               [&](std::array<int, D> coord) { u[coord] = sol_ld[coord] / scale; });
 	}
 };
-#if 0
-{
-	private:
-	int                                                n;
-	bool                                               initialized = false;
-	static bool                                        compareDomains();
-	double                                             lambda;
-	std::map<DomainK<D>, fftw_plan>                    plan1;
-	std::map<DomainK<D>, fftw_plan>                    plan2;
-	std::shared_ptr<ValVector<D>>                      f_copy;
-	std::shared_ptr<ValVector<D>>                      tmp;
-	std::shared_ptr<ValVector<D>>                      sol;
-	std::array<int, D + 1>                             npow;
-	std::map<DomainK<D>, std::valarray<double>>        denoms;
-	std::shared_ptr<Thunderegg::Schur::SchurHelper<D>> sh;
-	/**
-	 * @brief The patch opertar that we are solving for
-	 */
-	std::shared_ptr<const PatchOperator<D>> op;
-
-	public:
-	/**
-	 * @brief Construct a new FftwPatchSolver object
-	 *
-	 * @param op_in the Poisson PatchOperator that cooresponds to this DftPatchSolver
-	 */
-	explicit FftwPatchSolver(std::shared_ptr<const PatchOperator<D>> op_in)
-	: PatchSolver<D>(op_in->getDomain(), op_in->getGhostFiller()), op(op_in)
-	{
-		n = sh->getLengths()[0];
-		for (auto &sinfo : sh->getSchurInfoVector()) {
-			addPatch(*sinfo);
-		}
-	}
-	~FftwPatchSolver()
-	{
-		for (auto p : plan1) {
-			fftw_destroy_plan(p.second);
-		}
-		for (auto p : plan2) {
-			fftw_destroy_plan(p.second);
-		}
-	}
-	void solve(Thunderegg::Schur::SchurInfo<D> &sinfo, std::shared_ptr<const Vector<D>> f,
-	           std::shared_ptr<Vector<D>> u, std::shared_ptr<const Vector<D - 1>> gamma)
-	{
-		using namespace std;
-		const LocalData<D> f_view      = f->getLocalData(sinfo.pinfo->local_index);
-		LocalData<D>       f_copy_view = f_copy->getLocalData(0);
-		LocalData<D>       tmp_view    = tmp->getLocalData(0);
-
-		std::array<int, D> start, end;
-		start.fill(0);
-		end.fill(n - 1);
-
-		nested_loop<D>(start, end,
-		               [&](std::array<int, D> coord) { f_copy_view[coord] = f_view[coord]; });
-
-		op->addGhostToRHS(sinfo, gamma, f_copy->getLocalData(0));
-
-		fftw_execute(plan1[sinfo]);
-
-		tmp->vec /= denoms[sinfo];
-
-		if (sinfo.pinfo->neumann.all()) {
-			tmp->vec[0] = 0;
-		}
-
-		fftw_execute(plan2[sinfo]);
-
-		sol->vec /= pow(2.0 * n, D);
-
-		LocalData<D> u_view   = u->getLocalData(sinfo.pinfo->local_index);
-		LocalData<D> sol_view = sol->getLocalData(0);
-		nested_loop<D>(start, end,
-		               [&](std::array<int, D> coord) { u_view[coord] = sol_view[coord]; });
-	}
-	void solve(std::shared_ptr<const Vector<D>> f, std::shared_ptr<Vector<D>> u,
-	           std::shared_ptr<const Vector<D - 1>> gamma) override
-	{
-		for (auto &sinfo : sh->getSchurInfoVector()) {
-			solve(*sinfo, f, u, gamma);
-		}
-	}
-	void addPatch(Thunderegg::Schur::SchurInfo<D> &sinfo)
-	{
-		using namespace std;
-		if (!initialized) {
-			initialized = true;
-			for (size_t i = 0; i <= D; i++) {
-				npow[i] = (int) std::pow(n, i);
-			}
-			std::array<int, D> lengths;
-			lengths.fill(n);
-			f_copy = std::make_shared<ValVector<D>>(MPI_COMM_SELF, lengths, 0, 1);
-			tmp    = std::make_shared<ValVector<D>>(MPI_COMM_SELF, lengths, 0, 1);
-			sol    = std::make_shared<ValVector<D>>(MPI_COMM_SELF, lengths, 0, 1);
-		}
-
-		int           ns[D];
-		fftw_r2r_kind transforms[D];
-		fftw_r2r_kind transforms_inv[D];
-		if (!plan1.count(sinfo)) {
-			for (size_t i = 0; i < D; i++) {
-				ns[D - 1 - i] = n;
-				// x direction
-				if (sinfo.pinfo->isNeumann(Side<D>(2 * i))
-				    && sinfo.pinfo->isNeumann(Side<D>(2 * i + 1))) {
-					transforms[D - 1 - i]     = FFTW_REDFT10;
-					transforms_inv[D - 1 - i] = FFTW_REDFT01;
-				} else if (sinfo.pinfo->isNeumann(Side<D>(2 * i))) {
-					transforms[D - 1 - i]     = FFTW_REDFT11;
-					transforms_inv[D - 1 - i] = FFTW_REDFT11;
-				} else if (sinfo.pinfo->isNeumann(Side<D>(2 * i + 1))) {
-					transforms[D - 1 - i]     = FFTW_RODFT11;
-					transforms_inv[D - 1 - i] = FFTW_RODFT11;
-				} else {
-					transforms[D - 1 - i]     = FFTW_RODFT10;
-					transforms_inv[D - 1 - i] = FFTW_RODFT01;
-				}
-			}
-
-			plan1[sinfo] = fftw_plan_r2r(D, ns, &f_copy->vec[0], &tmp->vec[0], transforms,
-			                             FFTW_MEASURE | FFTW_DESTROY_INPUT);
-			plan2[sinfo] = fftw_plan_r2r(D, ns, &tmp->vec[0], &sol->vec[0], transforms_inv,
-			                             FFTW_MEASURE | FFTW_DESTROY_INPUT);
-		}
-
-		if (!denoms.count(sinfo)) {
-			valarray<double> &denom = denoms[sinfo];
-			denom.resize(pow(n, D));
-
-			valarray<double> ones(pow(n, D - 1));
-			ones = 1;
-
-			for (size_t i = 0; i < D; i++) {
-				valarray<size_t> sizes(D - 1);
-				sizes = n;
-				valarray<size_t> strides(D - 1);
-				for (size_t sinfo = 1; sinfo < D; sinfo++) {
-					strides[sinfo - 1] = pow(n, (i + sinfo) % D);
-				}
-				double h = sinfo.pinfo->spacings[i];
-
-				if (sinfo.pinfo->isNeumann(Side<D>(i * 2))
-				    && sinfo.pinfo->isNeumann(Side<D>(i * 2 + 1))) {
-					for (int xi = 0; xi < n; xi++) {
-						denom[gslice(xi * pow(n, i), sizes, strides)]
-						-= 4 / (h * h) * pow(sin(xi * M_PI / (2 * n)), 2) * ones;
-					}
-				} else if (sinfo.pinfo->isNeumann(Side<D>(i * 2))
-				           || sinfo.pinfo->isNeumann(Side<D>(i * 2 + 1))) {
-					for (int xi = 0; xi < n; xi++) {
-						denom[gslice(xi * pow(n, i), sizes, strides)]
-						-= 4 / (h * h) * pow(sin((xi + 0.5) * M_PI / (2 * n)), 2) * ones;
-					}
-				} else {
-					for (int xi = 0; xi < n; xi++) {
-						denom[gslice(xi * pow(n, i), sizes, strides)]
-						-= 4 / (h * h) * pow(sin((xi + 1) * M_PI / (2 * n)), 2) * ones;
-					}
-				}
-			}
-
-			denom += lambda;
-		}
-	}
-	std::shared_ptr<Thunderegg::Schur::PatchSolver<D>>
-	getNewPatchSolver(GMG::CycleFactoryCtx<D> ctx)
-	{
-		return std::shared_ptr<Thunderegg::Schur::PatchSolver<D>>(new FftwPatchSolver(ctx.sh));
-	}
-};
-#endif
 extern template class FFTWPatchSolver<2>;
 extern template class FFTWPatchSolver<3>;
 } // namespace Poisson
