c25b0d0453b5f1891663bd01146c38a98834a78a
Scott Aiton <scott@aiton.net>
2020-10-06 11:11:29 -0600

    (#58) BiLinearGhostFiller now supports multiple components

--- src/ThunderEgg/BiLinearGhostFiller.h
+++ src/ThunderEgg/BiLinearGhostFiller.h
@@ -36,41 +36,48 @@ class BiLinearGhostFiller : public MPIGhostFiller<2>
 	                               const std::vector<LocalData<2>> &nbr_datas, const Side<2> side,
 	                               const NbrType nbr_type, const Orthant<2> orthant) const override
 	{
-		auto &local_data = local_datas[0];
-		auto &nbr_data   = nbr_datas[0];
 		switch (nbr_type) {
 			case NbrType::Normal: {
-				auto local_slice = local_data.getSliceOnSide(side);
-				auto nbr_ghosts  = nbr_data.getGhostSliceOnSide(side.opposite(), 1);
-				nested_loop<1>(
-				nbr_ghosts.getStart(), nbr_ghosts.getEnd(),
-				[&](const std::array<int, 1> &coord) { nbr_ghosts[coord] = local_slice[coord]; });
+				for (int c = 0; c < local_datas.size(); c++) {
+					auto local_slice = local_datas[c].getSliceOnSide(side);
+					auto nbr_ghosts  = nbr_datas[c].getGhostSliceOnSide(side.opposite(), 1);
+					nested_loop<1>(nbr_ghosts.getStart(), nbr_ghosts.getEnd(),
+					               [&](const std::array<int, 1> &coord) {
+						               nbr_ghosts[coord] = local_slice[coord];
+					               });
+				}
 			} break;
 			case NbrType::Coarse: {
-				auto nbr_info    = pinfo->getCoarseNbrInfo(side);
-				auto local_slice = local_data.getSliceOnSide(side);
-				auto nbr_ghosts  = nbr_data.getGhostSliceOnSide(side.opposite(), 1);
-				int  offset      = 0;
+				auto nbr_info = pinfo->getCoarseNbrInfo(side);
+				int  offset   = 0;
 				if (orthant.collapseOnAxis(side.getAxisIndex()) == Orthant<1>::upper()) {
 					offset = pinfo->ns[!side.getAxisIndex()];
 				}
-				nested_loop<1>(
-				nbr_ghosts.getStart(), nbr_ghosts.getEnd(), [&](const std::array<int, 1> &coord) {
-					nbr_ghosts[{(coord[0] + offset) / 2}] += 2.0 / 3.0 * local_slice[coord];
-				});
+				for (int c = 0; c < local_datas.size(); c++) {
+					auto local_slice = local_datas[c].getSliceOnSide(side);
+					auto nbr_ghosts  = nbr_datas[c].getGhostSliceOnSide(side.opposite(), 1);
+					nested_loop<1>(nbr_ghosts.getStart(), nbr_ghosts.getEnd(),
+					               [&](const std::array<int, 1> &coord) {
+						               nbr_ghosts[{(coord[0] + offset) / 2}]
+						               += 2.0 / 3.0 * local_slice[coord];
+					               });
+				}
 			} break;
 			case NbrType::Fine: {
-				auto nbr_info    = pinfo->getFineNbrInfo(side);
-				auto local_slice = local_data.getSliceOnSide(side);
-				auto nbr_ghosts  = nbr_data.getGhostSliceOnSide(side.opposite(), 1);
-				int  offset      = 0;
+				auto nbr_info = pinfo->getFineNbrInfo(side);
+				int  offset   = 0;
 				if (orthant.collapseOnAxis(side.getAxisIndex()) == Orthant<1>::upper()) {
 					offset = pinfo->ns[!side.getAxisIndex()];
 				}
-				nested_loop<1>(
-				nbr_ghosts.getStart(), nbr_ghosts.getEnd(), [&](const std::array<int, 1> &coord) {
-					nbr_ghosts[coord] += 2.0 / 3.0 * local_slice[{(coord[0] + offset) / 2}];
-				});
+				for (int c = 0; c < local_datas.size(); c++) {
+					auto local_slice = local_datas[c].getSliceOnSide(side);
+					auto nbr_ghosts  = nbr_datas[c].getGhostSliceOnSide(side.opposite(), 1);
+					nested_loop<1>(nbr_ghosts.getStart(), nbr_ghosts.getEnd(),
+					               [&](const std::array<int, 1> &coord) {
+						               nbr_ghosts[coord]
+						               += 2.0 / 3.0 * local_slice[{(coord[0] + offset) / 2}];
+					               });
+				}
 			} break;
 		}
 	}
@@ -78,40 +85,42 @@ class BiLinearGhostFiller : public MPIGhostFiller<2>
 	void fillGhostCellsForLocalPatch(std::shared_ptr<const PatchInfo<2>> pinfo,
 	                                 const std::vector<LocalData<2>> &   local_datas) const override
 	{
-		auto &local_data = local_datas[0];
-		for (Side<2> side : Side<2>::getValues()) {
-			if (pinfo->hasNbr(side)) {
-				switch (pinfo->getNbrType(side)) {
-					case NbrType::Normal:
-						// nothing needs to be done
-						break;
-					case NbrType::Coarse: {
-						auto local_slice  = local_data.getSliceOnSide(side);
-						auto local_ghosts = local_data.getGhostSliceOnSide(side, 1);
-						int  offset       = 0;
-						if (pinfo->getCoarseNbrInfo(side).orth_on_coarse == Orthant<1>::upper()) {
-							offset = pinfo->ns[!side.getAxisIndex()];
-						}
-						nested_loop<1>(local_ghosts.getStart(), local_ghosts.getEnd(),
-						               [&](const std::array<int, 1> &coord) {
-							               local_ghosts[coord] += 2.0 / 3.0 * local_slice[coord];
-							               if ((coord[0] + offset) % 2 == 0) {
-								               local_ghosts[{coord[0] + 1}]
+		for (int c = 0; c < local_datas.size(); c++) {
+			for (Side<2> side : Side<2>::getValues()) {
+				if (pinfo->hasNbr(side)) {
+					switch (pinfo->getNbrType(side)) {
+						case NbrType::Normal:
+							// nothing needs to be done
+							break;
+						case NbrType::Coarse: {
+							auto local_slice  = local_datas[c].getSliceOnSide(side);
+							auto local_ghosts = local_datas[c].getGhostSliceOnSide(side, 1);
+							int  offset       = 0;
+							if (pinfo->getCoarseNbrInfo(side).orth_on_coarse
+							    == Orthant<1>::upper()) {
+								offset = pinfo->ns[!side.getAxisIndex()];
+							}
+							nested_loop<1>(
+							local_ghosts.getStart(), local_ghosts.getEnd(),
+							[&](const std::array<int, 1> &coord) {
+								local_ghosts[coord] += 2.0 / 3.0 * local_slice[coord];
+								if ((coord[0] + offset) % 2 == 0) {
+									local_ghosts[{coord[0] + 1}] += -1.0 / 3.0 * local_slice[coord];
+								} else {
+									local_ghosts[{coord[0] - 1}] += -1.0 / 3.0 * local_slice[coord];
+								}
+							});
+						} break;
+						case NbrType::Fine: {
+							auto local_slice  = local_datas[c].getSliceOnSide(side);
+							auto local_ghosts = local_datas[c].getGhostSliceOnSide(side, 1);
+							nested_loop<1>(local_ghosts.getStart(), local_ghosts.getEnd(),
+							               [&](const std::array<int, 1> &coord) {
+								               local_ghosts[coord]
 								               += -1.0 / 3.0 * local_slice[coord];
-							               } else {
-								               local_ghosts[{coord[0] - 1}]
-								               += -1.0 / 3.0 * local_slice[coord];
-							               }
-						               });
-					} break;
-					case NbrType::Fine: {
-						auto local_slice  = local_data.getSliceOnSide(side);
-						auto local_ghosts = local_data.getGhostSliceOnSide(side, 1);
-						nested_loop<1>(local_ghosts.getStart(), local_ghosts.getEnd(),
-						               [&](const std::array<int, 1> &coord) {
-							               local_ghosts[coord] += -1.0 / 3.0 * local_slice[coord];
-						               });
-					} break;
+							               });
+						} break;
+					}
 				}
 			}
 		}
