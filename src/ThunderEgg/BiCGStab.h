5c99f9ec90ab73720d7537411dac5d8a436398ae
Scott Aiton <scott@aiton.net>
2020-12-17 15:37:58 -0700

    Added output option to BiCGStab

--- src/ThunderEgg/BiCGStab.h
+++ src/ThunderEgg/BiCGStab.h
@@ -45,13 +45,17 @@ template <int D> class BiCGStab
 	 * @param x the initial LHS guess.
 	 * @param b the RHS vector.
 	 * @param Mr the right preconditioner. Set to nullptr if there is no right preconditioner.
+	 * @param timer the timer
+	 * @param output print output to std::cout
+	 * @param os the stream to output to
 	 *
 	 * @return the number of iterations
 	 */
 	static int solve(std::shared_ptr<VectorGenerator<D>> vg, std::shared_ptr<const Operator<D>> A,
 	                 std::shared_ptr<Vector<D>> x, std::shared_ptr<const Vector<D>> b,
 	                 std::shared_ptr<const Operator<D>> Mr = nullptr, int max_it = 1000,
-	                 double tolerance = 1e-12, std::shared_ptr<ThunderEgg::Timer> timer = nullptr)
+	                 double tolerance = 1e-12, std::shared_ptr<ThunderEgg::Timer> timer = nullptr,
+	                 bool output = false, std::ostream &os = std::cout)
 	{
 		std::shared_ptr<Vector<D>> resid = vg->getNewVector();
 		std::shared_ptr<Vector<D>> ms;
@@ -78,6 +82,11 @@ template <int D> class BiCGStab
 			return num_its;
 		}
 		double residual = resid->twoNorm() / r0_norm;
+		if (output) {
+			char buf[100];
+			sprintf(buf, "%5d %16.8e\n", num_its, residual);
+			os << std::string(buf);
+		}
 		while (residual > tolerance && num_its < max_it) {
 			if (timer) {
 				timer->start("Iteration");
@@ -133,6 +142,11 @@ template <int D> class BiCGStab
 				throw DivergenceError("BiCGSTab reached divergence criteria on iteration "
 				                      + std::to_string(num_its) + " with residual two norm "
 				                      + std::to_string(residual));
+			}
+			if (output) {
+				char buf[100];
+				sprintf(buf, "%5d %16.8e\n", num_its, residual);
+				os << std::string(buf);
 			}
 			if (timer) {
 				timer->stop("Iteration");
--- test/BiCGStab_MPI1.cpp
+++ test/BiCGStab_MPI1.cpp
@@ -25,6 +25,7 @@
 #include <ThunderEgg/BiLinearGhostFiller.h>
 #include <ThunderEgg/Poisson/StarPatchOperator.h>
 #include <ThunderEgg/ValVectorGenerator.h>
+#include <sstream>
 using namespace std;
 using namespace ThunderEgg;
 
@@ -65,6 +66,53 @@ TEST_CASE("BiCGStab solves poisson problem withing given tolerance", "[BiCGStab]
 	p_operator->apply(g_vec, residual);
 	residual->addScaled(-1, f_vec);
 	CHECK(residual->dot(residual) / f_vec->dot(f_vec) <= tolerance);
+}
+TEST_CASE("outputs iteration count and residual to output", "[BiCGStab]")
+{
+	string mesh_file = "mesh_inputs/2d_uniform_2x2_mpi1.json";
+	INFO("MESH FILE " << mesh_file);
+	DomainReader<2>       domain_reader(mesh_file, {32, 32}, 1);
+	shared_ptr<Domain<2>> domain = domain_reader.getCoarserDomain();
+
+	auto ffun = [](const std::array<double, 2> &coord) {
+		double x = coord[0];
+		double y = coord[1];
+		return -5 * M_PI * M_PI * sin(M_PI * y) * cos(2 * M_PI * x);
+	};
+	auto gfun = [](const std::array<double, 2> &coord) {
+		double x = coord[0];
+		double y = coord[1];
+		return sin(M_PI * y) * cos(2 * M_PI * x);
+	};
+
+	auto f_vec = ValVector<2>::GetNewVector(domain, 1);
+	DomainTools::SetValues<2>(domain, f_vec, ffun);
+	auto residual = ValVector<2>::GetNewVector(domain, 1);
+
+	auto g_vec = ValVector<2>::GetNewVector(domain, 1);
+
+	auto gf = make_shared<BiLinearGhostFiller>(domain);
+
+	auto p_operator = make_shared<Poisson::StarPatchOperator<2>>(domain, gf);
+	p_operator->addDrichletBCToRHS(f_vec, gfun);
+
+	double tolerance = 1e-7;
+
+	std::stringstream ss;
+
+	BiCGStab<2>::solve(make_shared<ValVectorGenerator<2>>(domain, 1), p_operator, g_vec, f_vec,
+	                   nullptr, 1000, tolerance, nullptr, true, ss);
+
+	INFO(ss.str());
+	int    prev_iteration;
+	double resid;
+	ss >> prev_iteration >> resid;
+	while (prev_iteration < 18) {
+		int iteration;
+		ss >> iteration >> resid;
+		CHECK(iteration == prev_iteration + 1);
+		prev_iteration = iteration;
+	}
 }
 TEST_CASE("giving a good initial guess reduces the iterations", "[BiCGStab]")
 {
