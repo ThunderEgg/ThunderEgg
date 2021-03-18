d16f8be8100c36e42797824373d08e422fe08d54
Scott Aiton <scott@aiton.net>
2021-03-18 14:45:09 -0600

    removed extra headers form test

--- test/GMG/DirectInterpolator_MPI1.cpp
+++ test/GMG/DirectInterpolator_MPI1.cpp
@@ -23,8 +23,6 @@
 #include "catch.hpp"
 #include <ThunderEgg/BiLinearGhostFiller.h>
 #include <ThunderEgg/DomainTools.h>
-#include <ThunderEgg/Experimental/DomGen.h>
-#include <ThunderEgg/Experimental/OctTree.h>
 #include <ThunderEgg/GMG/DirectInterpolator.h>
 #include <ThunderEgg/ValVector.h>
 using namespace std;
