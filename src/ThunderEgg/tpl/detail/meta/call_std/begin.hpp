9019c1bfb5f4f23e34124855eaf2545809c99108
Scott Aiton <scott@aiton.net>
2022-04-16 10:11:18 -0600

    single header

--- src/ThunderEgg/tpl/detail/meta/call_std/begin.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <ThunderEgg/tpl/detail/macro_scope.hpp>
-
-namespace ThunderEgg::tpl::nlohmann {
-NLOHMANN_CAN_CALL_STD_FUNC_IMPL(begin);
-} // namespace ThunderEgg::tpl::nlohmann
