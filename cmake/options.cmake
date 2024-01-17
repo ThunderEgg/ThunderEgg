c56fe0d1db76f9b5329d4732972e5df4ff2aa9fd
scivision <scivision@users.noreply.github.com>
2024-01-17 15:33:06 -0500

    consume p4est/sc via Git Submodule
    also make ThunderEgg options friendly to use as child project

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -7,7 +7,6 @@ option(fftw "allow the use the use of FFTW" on)
 cmake_dependent_option(fftw_required "fail if FFTW is not found" off "fftw" off)
 
 option(p4est "allow the use the use of p4est" on)
-cmake_dependent_option(p4est_external "build p4est library if not found on system" on "p4est" off)
 cmake_dependent_option(p4est_required "fail if p4est is not found" off "p4est" off)
 
 option(lapack "allow the use the use of lapack/blas" on)
@@ -15,9 +14,21 @@ cmake_dependent_option(lapack_required "fail if lapack/blas is not found" off "l
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 
+# options for libsc, p4est
+set(mpi true)
+
 # --- default install directory under build/local
+if(CMAKE_VERSION VERSION_LESS 3.21)
+  get_property(_not_top DIRECTORY PROPERTY PARENT_DIRECTORY)
+  if(NOT _not_top)
+    set(ThunderEgg_IS_TOP_LEVEL true)
+  endif()
+endif()
+
+option(ThunderEgg_BUILD_TESTING "build tests" ${ThunderEgg_IS_TOP_LEVEL})
+
 # users can specify like "cmake -B build -DCMAKE_INSTALL_PREFIX=~/mydir"
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+if(ThunderEgg_IS_TOP_LEVEL AND CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   # will not take effect without FORCE
   set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "Install top-level directory" FORCE)
 endif()
