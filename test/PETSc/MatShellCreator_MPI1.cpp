4ebb21113bc95e44178ed2c05a61fdaa3b0a4287
Scott Aiton <scott@aiton.net>
2025-03-25 01:21:22 -0600

    iwyu update petsc tests

--- test/PETSc/MatShellCreator_MPI1.cpp
+++ test/PETSc/MatShellCreator_MPI1.cpp
@@ -19,10 +19,18 @@
  ***************************************************************************/
 
 #include "../utils/DomainReader.h"
-#include <ThunderEgg/DomainTools.h>
 #include <ThunderEgg/PETSc/MatShellCreator.h>
 
+#include <ThunderEgg/Domain.h>
+#include <ThunderEgg/DomainTools.h>
+#include <ThunderEgg/Operator.h>
+#include <ThunderEgg/Vector.h>
 #include <doctest.h>
+#include <mpi.h>
+#include <petscmat.h>
+#include <petscsys.h>
+#include <petscvec.h>
+#include <string>
 
 using namespace std;
 using namespace ThunderEgg;
